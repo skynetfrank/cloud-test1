faf810dbbee145edcb9d58a530ee3568422afea4
skynetfrank <frank.uah@gmail.com>
2021-12-25 10:39:27 -0400

    test cloudinary-vercel

---
+++ server.js
@@ -0,0 +1,39 @@
+const { cloudinary } = require('./utils/cloudinary');
+const express = require('express');
+const app = express();
+var cors = require('cors');
+
+app.use(express.static('public'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cors());
+
+app.get('/api/images', async (req, res) => {
+  const { resources } = await cloudinary.search
+    .expression('folder:carrito22')
+    .sort_by('public_id', 'desc')
+    .max_results(30)
+    .execute();
+  const publicIds = resources.map(file => file.public_id);
+  res.send(publicIds);
+});
+
+app.post('/api/upload', async (req, res) => {
+  try {
+    const fileStr = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
+      upload_preset: 'carrito22_preset',
+    });
+    console.log(uploadResponse);
+    res.json({ msg: 'Cargado a cloudinary ok YO!' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ err: 'Server: hubo un error al intentar subir a cloudinary' });
+  }
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
---
+++ utils/cloudinary.js
@@ -0,0 +1,13 @@
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env'),
+});
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = { cloudinary };
