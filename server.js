18d8504755d8c4b35279c8a43906116b12625ea7
skynetfrank <frank.uah@gmail.com>
2021-12-25 11:27:56 -0400

    test cloudinary-vercel2

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { cloudinary } = require('./utils/cloudinary');
+import path from 'path';
 const express = require('express');
 const app = express();
 var cors = require('cors');
@@ -31,6 +32,11 @@ app.post('/api/upload', async (req, res) => {
     res.status(500).json({ err: 'Server: hubo un error al intentar subir a cloudinary' });
   }
 });
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 
 const port = process.env.PORT || 3001;
 
