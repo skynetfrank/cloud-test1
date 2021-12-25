3a0f4ff7f3b3e4b0f479b0e0a4bbb1aba8651470
skynetfrank <frank.uah@gmail.com>
2021-12-25 10:47:40 -0400

    test cloudinary-vercel

---
+++ frontend/build/precache-manifest.c727594a8c19e04c2630717e8f5c2f24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47ad4a73faf94de9ee85434d0d951bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5ce75ad9cd586e67fe6",
+    "url": "/static/css/main.2e5d3189.chunk.css"
+  },
+  {
+    "revision": "4fdf962c47c91ff17846",
+    "url": "/static/js/2.08c8527c.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.08c8527c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ce75ad9cd586e67fe6",
+    "url": "/static/js/main.787910b3.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
